004b65eeefb857176fa53fac676751f0073c5e93
sednor-0 <company@sednor.net>
2022-04-18 23:16:50 +0200

    BE integration

--- src/services/dbService.js
+++ src/services/dbService.js
@@ -2,5 +2,5 @@ import axios from '../config/axios';
 import { URLS } from '../constants/urls';
 
 export const fetchData = () => {
-  return axios.get(`${URLS.BOOK}?size=${Number.MAX_VALUE}&page=0`);
+  return axios.get(`${URLS.BOOK}?size=1000000&page=0`);
 };
